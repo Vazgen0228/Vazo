faada34d856304e5acb10153e965bd886093c3b4
Вазген Мартиросян <149735817+Vazgen0228@users.noreply.github.com>
2023-11-06 20:12:14 +0300

    Update Расчёт баланса студентов.sql

--- Расчёт баланса студентов.sql
+++ Расчёт баланса студентов.sql
@@ -1,1 +1,10 @@
-
+select sum("runtimeMinutes")/60 as sum_movie
+from imdb.title_basics b 
+join imdb.title_ratings r 
+on r.tconst = b.tconst 
+where genres like '%Animation%'
+        and "averageRating" > 8
+        and "startYear" >= 1990
+        and ("titleType" like 'movie'
+        or "titleType" like 'tvSeries'
+        or "titleType" like 'tvMiniSeries')
